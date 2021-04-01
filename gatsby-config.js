79806d7b0946f235e424fdd5a249a0f6d7551091
Jim Horn <jim.horn@asurion.com>
2021-04-01 07:41:24 -0700

    Add sass support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,20 @@
 module.exports = {
-  siteMetadata: {
-    title: "gatsby-slider-poc",
-  },
-  plugins: [
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
+    siteMetadata: {
+        title: 'gatsby-slider-poc',
     },
-  ],
+    plugins: [
+        'gatsby-plugin-styled-components',
+        'gatsby-plugin-image',
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp',
+        `gatsby-plugin-sass`,
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'images',
+                path: './src/images/',
+            },
+            __key: 'images',
+        },
+    ],
 };
